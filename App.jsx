e8e0ae3726af49388003ba233953b676cc9b216e
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-06-12 14:21:52 -0600

    First commit: instalacion de RN

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App() {
+
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
